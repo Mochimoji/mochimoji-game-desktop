254d563425b1571588a2a7460241b73aa3787ab0
makiroggers <makiroggers@gmail.com>
2017-11-14 18:21:23 -0800

    Fixed start scene logo and added null check for player.

--- Assets/Assets/Scripts/CameraRunner.cs
+++ Assets/Assets/Scripts/CameraRunner.cs
@@ -7,6 +7,8 @@ public class CameraRunner : MonoBehaviour {
 
     // Have the camera follow the Player.
 	void Update () {
-        transform.position = new Vector3(player.position.x + (float)8, (float)0.57, -10);
+        if (player != null) {
+            transform.position = new Vector3(player.position.x + (float)8, (float)0.57, -10);
+        }
 	}
 }
