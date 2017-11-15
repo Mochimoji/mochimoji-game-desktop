92e2f8aa4382d840d24723198275f7347616580d
Jennifer Feng <jjfeng520@gmail.com>
2017-11-15 15:32:24 -0800

    :fire: Merge scene and folder changes

---
+++ Assets/Scripts/GameStartScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameStartScript : MonoBehaviour {
+
+	void Start() 
+	{
+	}
+
+	void OnGUI()
+	{
+		// GUI.Label(new Rect(Screen.width / 2 - 40, 300, 80, 50), "Mochimoji");
+
+		if(GUI.Button(new Rect(Screen.width / 2 - 60, 450, 100, 40), "NEW GAME"))
+		{
+			SceneManager.LoadSceneAsync("MainScene", LoadSceneMode.Single);
+		}
+	}
+}
