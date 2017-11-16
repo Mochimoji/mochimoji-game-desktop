dcee22c08014b432abebf981d243b0b27c8dfe37
Jennifer Feng <jjfeng520@gmail.com>
2017-11-16 11:58:02 -0800

    :fire: testing git for unity
    commit gameover scene

---
+++ Assets/Scripts/GameOverScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverScript : MonoBehaviour
+{
+
+    void Start()
+    {
+    }
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(Screen.width / 2 - 60, 700, 100, 40), "PLAY AGAIN"))
+        {
+            SceneManager.LoadSceneAsync("MainScene", LoadSceneMode.Single);
+        }
+    }
+}
---
+++ Assets/Scripts/WallScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallScript : MonoBehaviour
+{
+    [SerializeField]
+    float x_offset;
+
+    [SerializeField]
+    float y_offset;
+    public Transform player;
+
+    public float moveSpeed = 1f;
+
+    private bool wall = true;
+
+    // Have the wall follow the Player only if Player moves ahead.
+    void Update()
+    {
+        //if (player.position.x - transform.position.x >= 100)
+        //{
+        //    // transform.position = new Vector3(player.position.x + (float)8 + x_offset, (float)0.57 + y_offset, -10);
+        //    transform.position = new Vector3(player.position.x + (float)8, (float)0.9339023, (float)-0.5319953);
+        //}
+        if (wall == true)
+        {
+            if (player.position.x - transform.position.x >= (float)200)
+            {
+                transform.position = new Vector3(player.position.x - (float)8, (float)0.9339023, (float)-0.5319953);
+                transform.position += transform.forward * Time.deltaTime * moveSpeed;
+            }
+
+            if (player == null)
+            {
+                wall = false;
+            }
+        }
+    }
+}
